f4745ce6f221ed70b9d1dfb3ed1bc2e50370253a
Ubuntu <ubuntu@ip-172-31-28-203.us-west-2.compute.internal>
2013-12-11 04:01:56 +0000

    Connecting to GooglePlaces via controller and returning results

--- app/controllers/google_places_controller.rb
+++ app/controllers/google_places_controller.rb
@@ -1,5 +1,10 @@
-class GooglePlacesController < ApplicationController
-   def home
-      @client = GooglePlaces::Client.new(AIzaSyCkAVK_RgfGtiuhtXhDCj1exNMaifXDJ80)
+ class GooglePlacesController < ApplicationController
+   def index
+   end
+
+   def show
+      @API_Key = 'AIzaSyCkAVK_RgfGtiuhtXhDCj1exNMaifXDJ80'
+      @client = GooglePlaces::Client.new(@API_Key)
+      @spotList = @client.spots(-33.8670522, 151.1957362, :types => ['restaurant','food'])
    end
 end
