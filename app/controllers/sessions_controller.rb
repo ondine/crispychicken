28760e6dacc110c22bdbf80464ca2c0eaf2286bd
Asitha de Silva <asithade@gmail.com>
2013-12-17 11:23:41 +0000

    Complete Overhaul of UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,21 +9,21 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             sign_in user
-            redirect_to events_path
+            redirect_to events_path, :notice => "Welcome, #{user.firstname}!"
         else
-            flash.now[:error] = 'Invalid email/password combination'
-            redirect_to root_url
+            flash.now[:error] = 'Invalid email/password combination!'
+            redirect_to root_url, :alert => "Invalid Email/Password Combination!"
         end		
 	end
 
 	def login
 		user = User.from_omniauth(env["omniauth.auth"])
 		session[:user_id] = user.id
-		redirect_to events_path
+		redirect_to events_path, :notice => "Welcome, #{user.firstname}!"
 	end
 
 	def destroy
 		session[:user_id] = nil
-		redirect_to root_url
+		redirect_to root_url, :notice => "Come back soon!"
 	end
 end
